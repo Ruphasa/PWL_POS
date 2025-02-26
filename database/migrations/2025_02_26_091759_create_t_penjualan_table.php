86c75eb87de41df1e37bc6c33ba3d60f632b907d
Ruphasa Mafahl <rfrizqifauzan@gmail.com>
2025-02-26 16:31:30 +0700

    praktikum 2.2

---
+++ database/migrations/2025_02_26_091759_create_t_penjualan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('barang_id')->index();
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('pembelian', 50);
+            $table->string('penjualan_kode', 20);
+            $table->dateTime('penjualan_tanggal');
+
+            // Mendefinisikan Foreign Key pada kolom barang_id mengacu pada kolom barang_id di tabel m_barang
+            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
+            // Mendefinisikan Foreign Key pada kolom user_id mengacu pada kolom user_id di tabel m_user
+            $table->foreign('user_id')->references('user_id')->on('m_user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_penjualan');
+    }
+};
