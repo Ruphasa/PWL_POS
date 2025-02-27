59adcb925c3b72409c0e3a4e4c811097a902aa0e
Ruphasa Mafahl <rfrizqifauzan@gmail.com>
2025-02-27 07:50:02 +0700

    Praktikum 2.3

--- database/migrations/2025_02_26_091122_create_t_stok_table.php
+++ database/migrations/2025_02_26_091122_create_t_stok_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id('stok_id');
             $table->unsignedBigInteger('supplier_id')->index();
             $table->unsignedBigInteger('barang_id')->index();
-            $table->unsignedBigInteger('used_id')->index();
-            $table->integer('stok_harga');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->dateTime('stok_tanggal');
             $table->integer('stok_jumlah');
 
             // Mendefinisikan Foreign Key pada kolom supplier_id mengacu pada kolom supplier_id di tabel m_supplier
@@ -24,7 +24,7 @@ return new class extends Migration
             // Mendefinisikan Foreign Key pada kolom barang_id mengacu pada kolom barang_id di tabel m_barang
             $table->foreign('barang_id')->references('barang_id')->on('m_barang');
             // Mendefinisikan Foreign Key pada kolom used_id mengacu pada kolom user_id di tabel m_user
-            $table->foreign('used_id')->references('user_id')->on('m_user');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
 
--- database/migrations/2025_02_26_091759_create_t_penjualan_table.php
+++ database/migrations/2025_02_26_091759_create_t_penjualan_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
     {
         Schema::create('t_penjualan', function (Blueprint $table) {
             $table->id('penjualan_id');
-            $table->unsignedBigInteger('barang_id')->index();
             $table->unsignedBigInteger('user_id')->index();
-            $table->string('pembelian', 50);
+            $table->string('pembeli', 50);
             $table->string('penjualan_kode', 20);
             $table->dateTime('penjualan_tanggal');
 
-            // Mendefinisikan Foreign Key pada kolom barang_id mengacu pada kolom barang_id di tabel m_barang
-            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
             // Mendefinisikan Foreign Key pada kolom user_id mengacu pada kolom user_id di tabel m_user
             $table->foreign('user_id')->references('user_id')->on('m_user');
         });
