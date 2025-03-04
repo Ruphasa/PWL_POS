a4c7f05eebef842d395a316874373dd2134dec15
Ruphasa Mafahl <rfrizqifauzan@gmail.com>
2025-03-04 23:28:15 +0700

    sudah sampai 4.4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,7 +10,7 @@
 
 <body>
     <h1>Data User</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
+    {{-- <table border="1" cellpadding="2" cellspacing="0">
         <tr>
             <th>ID</th>
             <th>Username</th>
@@ -23,6 +23,15 @@
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
         </tr>
+    </table> --}}
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>Jumlah Pengguna</th>
+        </tr>
+        <tr>
+            <td>{{ $data }}</td>
+        </tr>
+    </table>
 </body>
 
 </html>
