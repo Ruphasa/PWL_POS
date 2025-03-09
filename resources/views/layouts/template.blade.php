1cf36b395dfd304bbf9ce525d1e461e59a65bfd2
Ruphasa Mafahl <rfrizqifauzan@gmail.com>
2025-03-09 23:30:20 +0700

    SAN!

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,13 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'PWL Laravel Starter Code')}}</title>
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{ asset('Adminlte/plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="{{ asset('Adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+    <link rel="stylesheet"
+        href="{{ asset('Adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('Adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('Adminlte/dist/css/adminlte.min.css')}}">
+
+    @stack('css')
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -53,12 +62,29 @@
     <!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="{{ asset('Adminlte/plugins/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('Adminlte/plugins/jquery/jquery.min.js') }}"></script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('Adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{ asset('Adminlte/dist/js/adminlte.min.js')}}"></script>
-
+    <script src="{{ asset('Adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <!--DataTables & Plugins -->
+    <script src="{{ asset('Adminlte/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/jszip/jszip.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/pdfmake/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+    <script src="{{ asset('Adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+    <!--AdminLTE App -->
+    <script src="{{ asset('Adminlte/dist/js/adminlte.min.js') }}"></script>
+    <script>
+        // Untuk mengirimkan token Laravel CSRF pada setiap request ajax
+        $.ajaxSetup({ headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') } });
+    </script>
+    @stack('js') <!-- Digunakan untuk memanggil custom js dari perintah push('js') pada masing-masing view -->
 </body>
 
 </html>
